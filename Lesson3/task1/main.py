d74ec9bb293f96286f5ab0ef98d022e652ba14e1
Andrew Kobiz <andrewkobiz@gmail.com>
2023-04-11 11:01:22 +0300

    Added basic calculator with functions

---
+++ Lesson3/task1/main.py
@@ -0,0 +1,43 @@
+from math import sqrt
+
+def askNumbers():
+    return int(input('First number: ')), int(input('Second number: '))
+
+def plusNumbers(first, second):
+    return first + second
+
+def minusNumbers(first, second):
+    return first - second
+
+def divideNumbers(first, second):
+    return first / second
+
+def multiplyNumbers(first, second):
+    return first * second
+
+def powerNumbers(first, second):
+    return first**2, second**2
+
+def sqrtNumbers(first, second):
+    return round(sqrt(first), 2), round(sqrt(second), 2)
+
+def chooseFunction(first, second, choice):
+    match choice:
+        case 1: return plusNumbers(first, second)
+        case 2: return minusNumbers(first, second)
+        case 3: return divideNumbers(first, second)
+        case 4: return multiplyNumbers(first, second)
+        case 5: return powerNumbers(first, second)
+        case 6: return sqrtNumbers(first, second)
+        case _: return None
+
+def main():
+    userChoice = 1
+    while userChoice != 0:
+        firstNumber, secondNumber = askNumbers()
+        print("\n1.Plus two numbers\n2.Minus two numbers\n3.Divide two numbers\n4.Multiply two numbers\n5.Find a power of two numbers\n6.Find a root of two numbers\n0.Exit\n")
+        userChoice = int(input("Choose an operation to do: "))
+        print("Result:", chooseFunction(firstNumber, secondNumber, userChoice))
+
+if __name__ == "__main__":
+    main()
