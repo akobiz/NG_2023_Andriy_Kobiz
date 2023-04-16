151585dd737ffd3a0b337aee7cf6e1b1e32154a6
Andrew Kobiz <andrewkobiz@gmail.com>
2023-04-16 16:42:09 +0300

    Got rid of creating a whole bunch of lists in advance

--- Lesson2/task3/main.py
+++ Lesson2/task3/main.py
@@ -4,12 +4,8 @@ lists = []
 nunqLists = []
 
 for each in range(255):
-    lists.append([])
-    nunqLists.append([])
-
-for each in range(len(lists)):
-    lists[each] = [randint(0, 6) for _ in range(5)]
-    nunqLists[each] = list(set([elem for elem in lists[each] if lists[each].count(elem) > 1]))
+    lists.append([randint(0, 6) for _ in range(5)])
+    nunqLists.append([list(set([elem for elem in lists[each] if lists[each].count(elem) > 1]))])
 
 print(f"{lists}\n\n\n{nunqLists}")
 
