ef9c1cbe41755a4daf8e1da64559db0d88335535
Andrew Kobiz <andrewkobiz@gmail.com>
2023-04-16 16:36:44 +0300

    Changed the interface to a more understandable side

--- Lesson2/task1/main.py
+++ Lesson2/task1/main.py
@@ -1,7 +1,7 @@
-length = int(input('Enter length of something: '))
+length = int(input('Enter the length of the text, or numbers you\'ll be entering: '))
 
 while(True):
-    userSomething = input(f"Enter something with length {length}: ")
+    userSomething = input(f"Enter text or numbers with length {length}: ")
     if len(userSomething) == length:
         element = userSomething.count(input('Enter element to count him:'))
         print(f"Element count of you asked in your list = {element}")
