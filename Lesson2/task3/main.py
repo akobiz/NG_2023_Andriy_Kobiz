78dd77b8fd21917071afc414bc8bef74b2abf090
Andrew Kobiz <andrewkobiz@gmail.com>
2023-05-07 19:55:17 +0300

    Added opportunity to input elements of each pack

--- Lesson2/task3/main.py
+++ Lesson2/task3/main.py
@@ -4,7 +4,7 @@ lists = []
 nunqLists = []
 
 for each in range(255):
-    lists.append([randint(0, 6) for _ in range(5)])
+    lists.append(input(f"Enter {each} pack: "))
     nunqLists.append([list(set([elem for elem in lists[each] if lists[each].count(elem) > 1]))])
 
 print(f"{lists}\n\n\n{nunqLists}")
