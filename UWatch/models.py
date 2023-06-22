21ba2740036810ae6dce00b3d8a9cde4caf2eb4f
Andrew Kobiz <andrewkobiz@gmail.com>
2023-06-22 15:00:42 +0300

    added new model comments

--- UWatch/models.py
+++ UWatch/models.py
@@ -8,7 +8,9 @@ class Users(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     channel_name = db.Column(db.String(45), unique=True)
     password = db.Column(db.String(100))
+
     videos = db.relationship('Videos', backref='user', lazy=True)
+    comments = db.relationship('Comments', backref='user', lazy=True)
 
     def __init__(self, email, channel_name, password):
         self.email = email
@@ -29,6 +31,8 @@ class Videos(db.Model):
     dislike = db.Column(db.Integer, nullable=False)
     views = db.Column(db.Integer, nullable=False)
 
+    comments = db.relationship('Comments', backref='video', lazy=True)
+
     def __init__(self, user_id, video_url, video_name, like, dislike, views):
         self.user_id = user_id
         self.video_url = video_url
@@ -39,3 +43,19 @@ class Videos(db.Model):
 
     def __repr__(self):
         return str([self.id, self.video_url, self.like, self.dislike, self.views])
+    
+class Comments(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    video_url = db.Column(db.String(15), db.ForeignKey('videos.video_url'), nullable=False)
+    comment = db.Column(db.Text)
+
+    user = db.relationship('Users', backref='comments')
+    video = db.relationship('Videos', backref='comments')
+
+    def __init__(self, user_id, video_url, comment):
+        self.user_id = user_id
+        self.video_url = video_url
+        self.comment = comment
